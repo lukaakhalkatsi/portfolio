837dab978491f4d450d74ed2d75b6f5160a10447
Luka <luka.akhalkatsi.1@btu.edu.ge>
2024-06-04 12:06:54 -0700

    simple fixes

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -31,7 +31,7 @@ const RecentProjects = () => {
               </p>
               <div className="flex items-center justify-between mt-7 mb-3">
                 <div className="flex items-center">
-                  {/* {iconLists.map((icon, index) => (
+                  {iconLists.map((icon, index) => (
                     <div
                       key={icon}
                       className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex items-center justify-center"
@@ -39,7 +39,7 @@ const RecentProjects = () => {
                     >
                       <img src={icon} alt={icon} className="p-2" />
                     </div>
-                  ))} */}
+                  ))}
                 </div>
                 <div className="flex justify-center items-center">
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
